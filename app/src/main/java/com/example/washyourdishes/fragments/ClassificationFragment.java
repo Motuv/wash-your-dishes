864e234518900666bb4de581460fafdd6b0547b6
Motuv <piotrmiroslaw1@gmail.com>
2023-04-01 17:11:38 +0200

    Done classification fragment

--- app/src/main/java/com/example/washyourdishes/fragments/ClassificationFragment.java
+++ app/src/main/java/com/example/washyourdishes/fragments/ClassificationFragment.java
@@ -1,9 +1,13 @@
 package com.example.washyourdishes.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -11,11 +15,29 @@ import com.example.washyourdishes.R;
 
 
 public class ClassificationFragment extends Fragment {
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_classification, container, false);
+        Context context = this.getContext();
+        View view = inflater.inflate(R.layout.fragment_classification, null);
+        container = view.findViewById(R.id.classification);
+        for(int i=0; i<4; i+=1) {
+            TableRow tr = new TableRow(context);
+            ImageView iv = new ImageView(context);
+            iv.setScaleType(ImageView.ScaleType.FIT_XY);
+            iv.setImageResource(R.drawable.piotr);
+            iv.setAdjustViewBounds(true);
+            iv.setMaxWidth(300);
+
+            tr.addView(iv);
+
+            TextView tv = new TextView(context);
+            tv.setTextSize(100);
+            tv.setText("15");
+            tr.addView(tv);
+
+            container.addView(tr);
+        }
+        return view;
     }
 }
