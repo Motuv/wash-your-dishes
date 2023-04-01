ecf0b7ba298c70716cf58f9cc234baece61abad9
Motuv <piotrmiroslaw1@gmail.com>
2023-04-01 13:42:52 +0200

    rebuilding frontend

--- app/src/main/java/com/example/washyourdishes/MyViewPagerAdapter.java
+++ app/src/main/java/com/example/washyourdishes/MyViewPagerAdapter.java
@@ -5,9 +5,9 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 
-import com.example.washyourdishes.fragments.HomeFragment;
-import com.example.washyourdishes.fragments.NotificationFragment;
-import com.example.washyourdishes.fragments.SettingsFragment;
+import com.example.washyourdishes.fragments.ClassificationFragment;
+import com.example.washyourdishes.fragments.RulesFragment;
+import com.example.washyourdishes.fragments.ResourcesFragment;
 
 public class MyViewPagerAdapter  extends FragmentStateAdapter {
     public MyViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
@@ -19,13 +19,13 @@ public class MyViewPagerAdapter  extends FragmentStateAdapter {
     public Fragment createFragment(int position) {
         switch (position){
             case 0:
-                return new HomeFragment();
+                return new ClassificationFragment();
             case 1:
-                return new NotificationFragment();
+                return new RulesFragment();
             case 2:
-                return new SettingsFragment();
+                return new ResourcesFragment();
             default:
-                return new HomeFragment();
+                return new ClassificationFragment();
         }
     }
 
--- app/src/main/java/com/example/washyourdishes/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/washyourdishes/fragments/ProfileFragment.java
@@ -1,22 +1,21 @@
 package com.example.washyourdishes.fragments;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 import com.example.washyourdishes.R;
 
 
-public class SettingsFragment extends Fragment {
+public class ProfileFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
